ee03ea552e2c00e563f6ee9956cc147c7558b857
Salman <salmannajah123@gmail.com>
2025-06-11 14:58:26 +0530

    feat: add DarkModeToggle and Hamburger components;

---
+++ src/components/darkmode.tsx
@@ -0,0 +1,30 @@
+import { IconMoon, IconSun } from '@tabler/icons-react';
+import React, { useState } from 'react';
+import { motion } from 'framer-motion';
+
+const DarkModeToggle = () => {
+  const [theme, setTheme] = useState<'light' | 'dark'>('light');
+  const [rotation, setRotation] = useState(0);
+
+  const toggleTheme = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+    setRotation((prev) => prev - 180);
+  };
+
+  return (
+    <button onClick={toggleTheme} className="text-black dark:text-white cursor-pointer rounded-xl shadow-standard p-2">
+      <motion.div
+        animate={{ rotate: rotation }}
+        transition={{ duration: 0.4, ease: 'easeInOut' }}
+      >
+        {theme === 'dark' ? (
+          <IconSun className="w-5 h-5" />
+        ) : (
+          <IconMoon className="w-5 h-5" />
+        )}
+      </motion.div>
+    </button>
+  );
+};
+
+export default DarkModeToggle;
---
+++ src/components/hamburger.tsx
@@ -0,0 +1,12 @@
+import { IconMenu, IconMenu2, IconMenu3 } from '@tabler/icons-react'
+import React from 'react'
+
+const Hamburger = () => {
+  return (
+    <div>
+      <IconMenu3 className="text-black dark:text-white cursor-pointer shadow-standard rounded-lg p-2 w-9 h-9" />
+    </div>
+  )
+}
+
+export default Hamburger
