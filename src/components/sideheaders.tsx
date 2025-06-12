8e0da543fa6ee445a599aeab1ffef8245c4bab8b
Salman <salmannajah123@gmail.com>
2025-06-12 11:38:03 +0530

    fix: update styling in SideHeaders component for improved appearance

--- src/components/sideheaders.tsx
+++ src/components/sideheaders.tsx
@@ -5,16 +5,16 @@ import { motion } from 'motion/react'
 const SideHeaders = ({ children }: { children: React.ReactNode }) => {
   return (
     <motion.div
-      className="relative inline-block text-base mb-6 bg-neutral-200/70 dark:bg-neutral-700/70 text-neutral-600 dark:text-neutral-500 font-medium px-2 py-1"
+      className="relative inline-block text-base mb-6 bg-neutral-200/70 dark:bg-neutral-700/30 text-neutral-600 dark:text-neutral-300 font-medium py-0.5 px-1"
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       transition={{ duration: 0.9, ease: 'easeInOut', delay: 0.5 }}
     >
       <span className="relative z-10">{children}</span>
-      <div className="absolute -top-[2px] -left-[2px] w-[6px] h-[6px] rounded-full bg-neutral-500/40 dark:bg-neutral-300/40 z-0" />
-      <div className="absolute -top-[2px] -right-[2px] w-[6px] h-[6px] rounded-full bg-neutral-500/40 dark:bg-neutral-300/40 z-0" />
-      <div className="absolute -bottom-[2px] -left-[2px] w-[6px] h-[6px] rounded-full bg-neutral-500/40 dark:bg-neutral-300/40 z-0" />
-      <div className="absolute -bottom-[2px] -right-[2px] w-[6px] h-[6px] rounded-full bg-neutral-500/40 dark:bg-neutral-300/40 z-0" />
+      <div className="absolute -top-[2px] -left-[2px] w-[5px] h-[5px] rounded-full bg-neutral-500/30 dark:bg-neutral-300/20 z-0" />
+      <div className="absolute -top-[2px] -right-[2px] w-[5px] h-[5px] rounded-full bg-neutral-500/30 dark:bg-neutral-300/20 z-0" />
+      <div className="absolute -bottom-[2px] -left-[2px] w-[5px] h-[5px] rounded-full bg-neutral-500/30 dark:bg-neutral-300/20 z-0" />
+      <div className="absolute -bottom-[2px] -right-[2px] w-[5px] h-[5px] rounded-full bg-neutral-500/30 dark:bg-neutral-300/20 z-0" />
     </motion.div>
   );
 };
