588cc8440a96595c92b49a655c6d7748f5acd486
Salman <salmannajah123@gmail.com>
2025-06-13 22:33:30 +0530

    fix: update blog metadata and improve layout for better readability

---
+++ src/app/about/page.tsx
@@ -0,0 +1,26 @@
+import Container from "@/components/container";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+    title: "Salman Najah | About",
+    description: "Learn more about Salman Najah.",
+};
+
+export default async function AboutPage() {
+    return (
+        <div>
+            <Container className="min-h-screen pt-24 pb-8 border-neutral-200 dark:border-neutral-700 ">
+                <h1 className="text-3xl md:text-4xl font-bold text-primary dark:text-white tracking-tight">
+                    About Me
+                </h1>
+                <p className="mt-4 text-neutral-500 dark:text-neutral-400">
+                    I am a passionate web developer with a love for creating dynamic and user-friendly web applications. My journey in tech has been fueled by curiosity and a desire to learn. I enjoy tackling challenges and finding innovative solutions to problems.
+                </p>
+
+                <p className="mt-4 text-neutral-500 dark:text-neutral-400">
+                    Timeline coming soon
+                </p>
+            </Container>
+        </div>
+    );
+}
