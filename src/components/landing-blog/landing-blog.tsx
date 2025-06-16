b81b8258647cb8f77fcbb8dff6c839bf4cf0ad09
Salman <salmannajah123@gmail.com>
2025-06-16 23:47:33 +0530

    feat: create LandingBlogs component to display blog entries with dynamic links

---
+++ src/components/landing-blog/landing-blog.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import Container from '../container'
+import SideHeaders from '../sideheaders'
+import { fetchAllBlogs } from '@/lib/mdx';
+import { Link } from 'next-view-transitions';
+
+type Blog = {
+  slug: string;
+  title: string;
+  readTime: string;
+};
+
+const LandingBlogs = async ({ allBlogs } : { allBlogs: Blog[] }) => {
+  return (
+    <div>
+      <Container className='min-h-[200px] md:pb-18 pb-6 md:pt-6 pt-3'>
+        <SideHeaders>Some of my writings</SideHeaders>
+        <div className="flex flex-col gap-4 mt-8">
+          {allBlogs.map((blog, idx) => (
+            <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-standard hover:scale-101 transition-all duration-100 ease-in-out border-1 border-neutral-200 dark:border-neutral-700">
+              <div className="flex justify-between items-center">
+                <span className="font-bold">{blog?.title}</span>
+                <span className="text-sm text-neutral-500 dark:text-neutral-400">{blog?.readTime}</span>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </Container>
+    </div>
+  )
+}
+
+export default LandingBlogs
