76de29255144c678e35e3dfd6e7ad6ff89f37d19
Salman <salmannajah123@gmail.com>
2025-06-16 10:57:10 +0530

    feat: update NotFoundPage to redirect to home and improve UI

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,27 +2,27 @@
 
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
+import Container from "@/components/container";
 
 export default function NotFoundPage() {
   const router = useRouter();
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      router.replace("/blog"); // Auto redirect after 2 seconds
-    }, 2000);
+      router.replace("/");
+    }, 3000);
 
     return () => clearTimeout(timer);
   }, [router]);
 
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center text-center p-4">
-      <h1 className="text-2xl font-bold mb-2">Blog Not Found</h1>
-      <p className="text-neutral-600 dark:text-neutral-400">
-        The blog you’re looking for doesn’t exist. Redirecting to all blogs...
-      </p>
-      <p className="mt-4 text-sm text-blue-500 underline cursor-pointer" onClick={() => router.replace("/blog")}>
-        Go to All Blogs Now
-      </p>
+    <div className="min-h-screen flex justify-center text-center">
+      <Container className="pt-48">
+        <h1 className="text-2xl font-bold mb-2">Page Not Found</h1>
+        <p className="text-neutral-600 dark:text-neutral-400">
+          The page you’re looking for doesn’t exist. Redirecting...
+        </p>
+      </Container>
     </div>
   );
 }
