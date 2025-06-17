5a75fe88be2a2d3ae2bbae5d944d7b0828f8a2e8
Salman <salmannajah123@gmail.com>
2025-06-17 14:46:40 +0530

    feat: update LandingBlogs component to display latest blogs and improve layout

--- src/components/landing-blog/landing-blog.tsx
+++ src/components/landing-blog/landing-blog.tsx
@@ -6,12 +6,13 @@ import { Link } from 'next-view-transitions';
 
 const LandingBlogs = async () => {
   const allBlogs = await fetchAllBlogs();
+  const latestBlogs = allBlogs.slice(0, 2);
   return (
     <div>
-      <Container className='min-h-[200px] md:pb-18 pb-6 md:pt-6 pt-3'>
+      <Container className='min-h-[200px] md:pb-12 pb-6 md:pt-6 pt-3 border-b-1'>
         <SideHeaders>Some of my writings</SideHeaders>
         <div className="flex flex-col gap-4 mt-8">
-          {allBlogs.map((blog, idx) => (
+          {latestBlogs.map((blog, idx) => (
             <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-standard hover:scale-101 transition-all duration-100 ease-in-out border-1 border-neutral-200 dark:border-neutral-700">
               <div className="flex justify-between items-center">
                 <span className="font-bold">{blog?.title}</span>
@@ -19,6 +20,11 @@ const LandingBlogs = async () => {
               </div>
             </Link>
           ))}
+        </div>
+        <div className="mt-6 flex justify-start pl-1">
+          <Link href="/blog" className="text-sm text-neutral-600 dark:text-neutral-300 underline hover:no-underline transition">
+            See All Blogs →
+          </Link>
         </div>
       </Container>
     </div>
