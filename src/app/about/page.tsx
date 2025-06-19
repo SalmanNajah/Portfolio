ec1c6206f1caaa1968fd3b9a9ea5d80b93071fdc
Salman <salmannajah123@gmail.com>
2025-06-19 21:54:27 +0530

    fix build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -18,7 +18,7 @@ export default async function AboutPage() {
                 </h1>
                 <div className="md:justify-between mt-6 md:flex ">
                     <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base max-w-[40rem] pb-4">
-                            I'm a second-year engineering student with a deep interest in building for the web. I enjoy working across the full stack, diving into how systems function behind the scenes, and constantly experimenting with new technologies. Most of my learning comes from hands-on exploration - trying, failing, and building again. Recently, I’ve been exploring how AI can intersect with the web to create meaningful tools. I'm also actively contributing to open source and working on digital products of my own.
+                            I&#39;m a second-year engineering student with a deep interest in building for the web. I enjoy working across the full stack, diving into how systems function behind the scenes, and constantly experimenting with new technologies. Most of my learning comes from hands-on exploration - trying, failing, and building again. Recently, I&#39;ve been exploring how AI can intersect with the web to create meaningful tools. I&#39;m also actively contributing to open source and working on digital products of my own.
                     </p>
                     <Resume />
                 </div>
