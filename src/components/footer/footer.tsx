51afea4369cead2d490ec9b3ca46927efa8683b0
Salman <salmannajah123@gmail.com>
2025-06-27 22:27:13 +0530

    fix: Design of the footer

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -5,7 +5,7 @@ import { Link } from 'next-view-transitions'
 const Footer = () => {
     return (
         <footer>
-            <Container className='pt-8 pb-6 text-neutral-500 space-y-8 border-t-1 border-neutral-200 dark:border-neutral-700 '>
+            <Container className='pt-8 pb-6 text-neutral-500 space-y-8 border-t-1 border-neutral-200 dark:border-neutral-700'>
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-x-12 gap-y-5">
                     <ul className="list-none space-y-3">
                         <li><Link href="/" className="hover:text-neutral-800 dark:hover:text-neutral-300">Home</Link></li>
@@ -26,7 +26,7 @@ const Footer = () => {
                     </ul>
                 </div>
                 <div className='text-sm'>
-                    Built with love by <span className='font-bold'>Salman Najah</span>
+                    Designed and Developed by <Link href="/"><span className='font-bold'>Salman Najah</span></Link>
                 </div>
             </Container>
         </footer>
