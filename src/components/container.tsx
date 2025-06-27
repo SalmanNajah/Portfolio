b5e843f64d046d2219590a9a909f6423908c2e9c
Salman <salmannajah123@gmail.com>
2025-06-28 00:32:42 +0530

    add border to container

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -9,7 +9,7 @@ type ContainerProps = {
 const Container = ({ children, className }: ContainerProps) => {
     return (
         <>
-            <div className={cn("dark:bg-neutral-900 max-w-lg md:max-w-4xl w-full mx-auto bg-white px-4 md:px-10 min-h-full", className)}>{children}</div>
+            <div className={cn("dark:bg-neutral-900 max-w-lg md:max-w-4xl w-full mx-auto bg-white px-4 md:px-10 min-h-full border-x border-color-new", className)}>{children}</div>
         </>
     )
 }
