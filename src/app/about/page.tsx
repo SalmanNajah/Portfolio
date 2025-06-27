2e672753101d1eeef13c2c662b2b490dedd7be81
Salman <salmannajah123@gmail.com>
2025-06-27 22:27:00 +0530

    fix: Refine personal descriptions in About and Blog pages for clarity and conciseness

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,8 +17,12 @@ export default async function AboutPage() {
                     About Me
                 </h1>
                 <div className="md:justify-between mt-6 md:flex ">
-                    <p className="text-neutral-500 dark:text-neutral-400 text-sm md:text-base max-w-[40rem] pb-4">
-                            I&#39;m a second-year engineering student with a deep interest in building for the web. I enjoy working across the full stack, diving into how systems function behind the scenes, and constantly experimenting with new technologies. Most of my learning comes from hands-on exploration - trying, failing, and building again. Recently, I&#39;ve been exploring how AI can intersect with the web to create meaningful tools. I&#39;m also actively contributing to open source and working on digital products of my own.
+                    <p className="text-neutral-500 md:text-base max-w-[40rem] pb-4 text-base">
+                        I’m a second-year engineering student deeply interested in building for the web.
+                        I love exploring the full stack - especially how systems work behind the scenes.
+                        Most of what I’ve learned comes from trying, failing, and building again.
+                        Lately, I've been curious about how AI can intersect with the web to create meaningful tools.
+                        I’m also contributing to open source and building digital products on the side.
                     </p>
                     <Resume />
                 </div>
