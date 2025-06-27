49cf4691e3d5ae9085ac8ebdcd7a51dd16b6b71f
Salman <salmannajah123@gmail.com>
2025-06-28 00:32:28 +0530

    update border color 2

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -23,7 +23,7 @@ export default async function AllBlogsPage() {
                 <p className="text-neutral-500 pt-4 text-sm md:text-base max-w-2xl pb-4">I&#39;ve been programming for almost two years. Over the past year, I explored different technologies and built real-world projects. This blog is where I share what I&#39;ve learned and the challenges I&#39;ve faced along the way.</p>
                 <div className="flex flex-col gap-4 mt-8">
                     {allBlogs.map((blog, idx) => (
-                        <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-derek transition-all duration-100 ease-in-out border-1 border-color-new dark:border-color-new dark:hover:shadow-[var(--shadow-standard)]">
+                        <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-derek transition-all duration-100 ease-in-out  border-1 border-neutral-300 dark:border-neutral-700 dark:hover:shadow-[var(--shadow-standard)]">
                             <div className="flex justify-between items-center">
                             <span className="font-bold">{blog?.title}</span>  
                                 <span className="text-sm text-neutral-500 dark:text-neutral-400">{blog?.readTime}</span>
--- src/components/landing-blog/landing-blog.tsx
+++ src/components/landing-blog/landing-blog.tsx
@@ -9,11 +9,11 @@ const LandingBlogs = async () => {
   const latestBlogs = allBlogs.slice(0, 2);
   return (
     <div>
-      <Container className='min-h-full pb-6 md:pt-6 pt-3 '>
+      <Container className='min-h-full pb-6 md:pt-6 pt-3'>
         <SideHeaders>Some of my writings</SideHeaders>
         <div className="flex flex-col gap-4 mt-8">
           {latestBlogs.map((blog, idx) => (
-            <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-standard hover:scale-101 transition-all duration-100 ease-in-out border-1 border-color-new dark:border-color-new">
+            <Link key={idx} href={`/blog/${blog?.slug}`} className="bg-neutral-100 dark:bg-[#302730] p-4 rounded-lg hover:shadow-standard hover:scale-101 transition-all duration-100 ease-in-out border-1 border-neutral-300 dark:border-neutral-700">
               <div className="flex justify-between items-center">
                 <span className="font-bold">{blog?.title}</span>
                 <span className="text-sm text-neutral-500 dark:text-neutral-400">{blog?.readTime}</span>
