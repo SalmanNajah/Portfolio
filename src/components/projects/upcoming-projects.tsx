f518f5f0095a457a5672531ff66061ef0bc68a30
Salman <salmannajah123@gmail.com>
2025-06-30 22:14:56 +0530

    Improved shadow in projects card

--- src/components/projects/upcoming-projects.tsx
+++ src/components/projects/upcoming-projects.tsx
@@ -45,7 +45,7 @@ const UpcomingProjects = ({ limit }: { limit?: number }) => {
                 {displayedProjects.map((item, idx) => (
                     <motion.div
                         key={idx}
-                        className="bg-white dark:bg-[#1b1b1a] p-2 rounded-lg shadow-standard hover:shadow-derek dark:hover:shadow-[var(--shadow-standard)]"
+                        className="bg-white dark:bg-[#1b1b1a] p-2 rounded-lg shadow-standard dark:shadow-[var(--shadow-standard)] hover:shadow-derek dark:hover:shadow-[var(--shadow-derek)]"
                         initial={{ opacity: 0, filter: "blur(6px)", y: 10 }}
                         whileInView={{ opacity: 1, filter: "blur(0px)", y: 0 }}
                         transition={{ duration: 0.1 * (idx + 1), ease: "easeInOut" }}
