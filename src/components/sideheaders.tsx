3aa8a53148cb81217783824b6253e92c8423da28
Salman Najah <salmannajah123@gmail.com>
2025-10-31 23:34:12 +0530

    fix font

--- src/components/sideheaders.tsx
+++ src/components/sideheaders.tsx
@@ -5,7 +5,7 @@ import { cn } from '@/lib/utils';
 const SideHeaders = ({ children, className }: { children: React.ReactNode, className?: string }) => {
   return (
     <div className={cn("relative border-1 border-dashed border-color-new max-w-4xl mx-auto bg-container", className)}>
-      <div className='inline-block border-x-1 ml-6 md:ml-10 p-2 border-dashed border-color-new bg-side-header'>{children}</div>
+      <div className='inline-block border-x-1 ml-6 md:ml-10 p-2 border-dashed border-color-new bg-side-header font-semibold'>{children}</div>
     </div>
   );
 };
