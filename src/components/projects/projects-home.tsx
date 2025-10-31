ce13ebc7a6f2319c2a4b407e268dd993f1c7cb1e
Salman Najah <salmannajah123@gmail.com>
2025-10-31 23:26:46 +0530

    fix

--- src/components/projects/projects-home.tsx
+++ src/components/projects/projects-home.tsx
@@ -6,15 +6,18 @@ import { Link } from 'next-view-transitions'
 
 //Moved the content of the  Projects to ProjectContent component for better organization
 const ProjectHome = () => {
+  const limit = 4;
   return (
     <div>
       <SideHeaders>Projects</SideHeaders>
       <Container className="md:pt-6 pt-3 md:pb-8 pb-6 ">
-        <ProjectContent limit={4} />
+        <ProjectContent limit={limit} />
         <div className="mt-6 flex justify-start pl-1">
-          <Link href="/projects" className="text-sm text-neutral-600 dark:text-neutral-300 underline hover:no-underline transition">
-            See All Projects →
-          </Link>
+          {limit > 4 && (
+            <Link href="/projects" className="text-sm text-neutral-600 dark:text-neutral-300 underline hover:no-underline transition">
+              See All Projects →
+            </Link>
+          )}
         </div>
       </Container>
     </div>
