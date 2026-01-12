a816484e69b1358758bc53171be4215cd51cc16f
Salman Najah <salmannajah123@gmail.com>
2026-01-12 17:51:28 +0530

    contact cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import GithubWrapper from "@/components/github-wrapper/github-wrapper";
 import MainLanding from "@/components/main-landing";
 import ProjectHome from "@/components/projects/projects-home";
 // import UpcomingProjects from "@/components/projects/upcoming-projects";
-import ContactSection from "@/components/contact/contact-section";
 
 export default function Home() {
   return (
@@ -13,7 +12,6 @@ export default function Home() {
       <ProjectHome />
       {/* <UpcomingProjects limit={2} /> */}
       {/* <LandingBlogs /> */}
-      <ContactSection />
     </div>
   );
 }
